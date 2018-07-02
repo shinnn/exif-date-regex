dd1559a0bb25b58ce48a68fbe861053239604bd3
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-07-02 19:42:56 +0900

    remove a meaningless strict mode statement

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*!
  * exif-date-regex | ISC (c) Shinnosuke Watanabe
  * https://github.com/shinnn/exif-date-regex
@@ -7,6 +5,4 @@
 // Dinker Batra's code is used as reference for leap year validation
 // https://www.c-sharpcorner.com/forums/date-regular-expression-for-leap-year-date-validatation
 // (Terms: https://www.c-sharpcorner.com/termsconditions.aspx)
-var index = /^(?<year>\d{4}):(?<month>\d{2}):(?<date>(?:(?<=02:)0[1-9]|1\d|2[0-8])|(?:(?<=(?:(?:\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])0{2}):02:)29)|(?:(?<=(?:[469]|11):)(?:0[1-9]|[12]\d|30))|(?:(?<=(?:0[13578]|1[02]):)(?:0[1-9]|[12]\d|3[01]))) (?<hours>[0-1]\d|2[0-3]):(?<minutes>[0-5]\d):(?<seconds>[0-5]\d)$/;
-
-module.exports = index;
+module.exports=/^(?<year>\d{4}):(?<month>\d{2}):(?<date>(?:(?<=02:)0[1-9]|1\d|2[0-8])|(?:(?<=(?:(?:\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])0{2}):02:)29)|(?:(?<=(?:[469]|11):)(?:0[1-9]|[12]\d|30))|(?:(?<=(?:0[13578]|1[02]):)(?:0[1-9]|[12]\d|3[01]))) (?<hours>[0-1]\d|2[0-3]):(?<minutes>[0-5]\d):(?<seconds>[0-5]\d)$/;
