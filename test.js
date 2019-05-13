8d025bac0d7f8bcaa9547f40c5db7ffa396e8e9d
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-05-13 10:47:57 +0900

    update devDependencies
    Highlight: switch from tape to testit https://github.com/ForbesLindesay/testit

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const exifDateRegex = require('.');
-const test = require('tape');
+const {deepEqual, equal} = require('assert').strict;
 
-test('exifDateRegex()', t => {
-	t.deepEqual(
+const exifDateRegex = require('.');
+const test = require('testit');
+
+test('match a date in the Exif compatible format', () => {
+	deepEqual(
 		[...exifDateRegex.exec('2018:06:19 15:01:38')],
 		['2018:06:19 15:01:38', '2018', '06', '19', '15', '01', '38'],
-		'should match a date in the Exif compatible format.'
 	);
+});
 
-	t.deepEqual(
-		exifDateRegex.exec('3104:02:29 23:59:59').groups,
+test('assign each parsed values to the `group` property', () => {
+	deepEqual(
+		{...exifDateRegex.exec('3104:02:29 23:59:59').groups},
 		{
 			year: '3104',
 			month: '02',
@@ -19,39 +22,23 @@ test('exifDateRegex()', t => {
 			hours: '23',
 			minutes: '59',
 			seconds: '59'
-		},
-		'should assign each parsed values to the `group` property.'
+		}
 	);
+});
 
-	t.equal(
-		exifDateRegex.exec('2015:02:29 23:01:38'),
-		null,
-		'should take leap years into consideration.'
-	);
+test('take leap years into consideration', () => {
+	equal(exifDateRegex.exec('2015:02:29 23:01:38'), null);
+});
 
-	t.equal(
-		exifDateRegex.exec('10000:04:29 01:11:58'),
-		null,
-		'should invalidate years more than 9999.'
-	);
+test('invalidate years more than 9999', () => {
+	equal(exifDateRegex.exec('10000:04:29 01:11:58'), null);
+});
 
-	t.equal(
-		exifDateRegex.exec('1967:12:31 24:00:00'),
-		null,
-		'should validate time.'
-	);
+test('validate time', () => {
+	equal(exifDateRegex.exec('1967:12:31 24:00:00'), null);
+});
 
-	t.equal(
-		exifDateRegex.exec('\n2011:11:11 23:59:12'),
-		null,
-		'should invaldiate leading white spaces.'
-	);
-
-	t.equal(
-		exifDateRegex.exec('1991:01:01 00:00:00 '),
-		null,
-		'should invaldiate trailing white spaces.'
-	);
-
-	t.end();
+test('invaldiate leading and trailing white spaces', () => {
+	equal(exifDateRegex.exec('\n2011:11:11 23:59:12'), null);
+	equal(exifDateRegex.exec('1991:01:01 00:00:00 '), null);
 });
